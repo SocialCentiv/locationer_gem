d4780c7ce2be36aca6954b2d252a464ef38b343f
Juan Leal <juan@hiplogiq.com>
2013-12-18 11:46:51 -0600

    not using this table no more

--- app/models/locationer/geo_data.rb
+++ app/models/locationer/geo_data.rb
@@ -1,5 +1,19 @@
-module Locationer
-  class GeoData < ActiveRecord::Base
-    #attr_accessible :country, :city_name, :latitude, :longitude, :state
-  end
-end
+# == Schema Information
+#
+# Table name: locationer_geo_data
+#
+#  id         :integer          not null, primary key
+#  country    :string(255)
+#  city_name  :string(255)
+#  state      :string(255)
+#  latitude   :float
+#  longitude  :float
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+# module Locationer
+#   class GeoData < ActiveRecord::Base
+#     #attr_accessible :country, :city_name, :latitude, :longitude, :state
+#   end
+# end
