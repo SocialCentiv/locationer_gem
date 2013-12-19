b2bea1145af6bffe42df0404ecf7ef0a552bcc6e
Juan Leal <juan@hiplogiq.com>
2013-12-19 16:51:44 -0600

    A helper method for generating factory girl code

--- lib/locationer/location_helper.rb
+++ lib/locationer/location_helper.rb
@@ -5,5 +5,24 @@ module Locationer
       return [] unless self.alternatenames
       self.alternatenames.split(",")
     end
+
+    def factory_girl_formatter
+      format = ->(_val, col) do 
+        if col == "alternatenames"
+          list = _val.split(",")[0..3].compact.join(",")
+          "\"#{list}\""
+        elsif _val.is_a? String
+          "\"#{_val}\""
+        else
+          _val
+        end
+      end
+
+      self.attributes.each do |col,val|
+        puts "#{col} #{format.call(val,col)}" if val && col != "id"
+      end
+
+      nil
+    end
   end
 end
