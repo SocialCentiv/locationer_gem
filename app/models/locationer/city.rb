faa1749e93b95f5c90bb32be67f638c55f62e09c
Juan Leal <juan@hiplogiq.com>
2013-12-19 23:22:53 -0600

    annotated

--- app/models/locationer/city.rb
+++ app/models/locationer/city.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: locationer_locations
+#
+#  id                :integer          not null, primary key
+#  name              :string(255)
+#  asciiname         :string(255)
+#  alternatenames    :text
+#  latitude          :float
+#  longitude         :float
+#  feature_class     :string(255)
+#  feature_code      :string(255)
+#  admin1_code       :string(255)
+#  population        :integer
+#  timezone          :string(255)
+#  modification_date :datetime
+#
+
 module Locationer
   class City < Location
     include LocationHelper
