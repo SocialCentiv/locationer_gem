689fc890718a91b8c492b0dc1e2070a924443d02
Juan Leal <juan@hiplogiq.com>
2013-12-19 23:24:12 -0600

    load city_json_decorator

--- lib/locationer.rb
+++ lib/locationer.rb
@@ -1,6 +1,7 @@
 require "locationer/location_helper"
 require "locationer/country_json_decorator"
 require "locationer/subdivision_json_decorator"
+require "locationer/city_json_decorator"
 require "locationer/engine"
 require "locationer/geoname_parser"
 
