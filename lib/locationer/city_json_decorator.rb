dd674900265b4a93d566c4d1da0e7f96e161fe83
Juan Leal <juan@hiplogiq.com>
2013-12-19 23:22:15 -0600

    Added decorator

---
+++ lib/locationer/city_json_decorator.rb
@@ -0,0 +1,31 @@
+
+module Locationer
+  class CityJsonDecorator
+    def self.cities_to_json(cities)
+      (cities.collect {|city| self.new(city).to_hash}).to_json
+    end
+
+    def initialize(city)
+      @city = city
+    end
+
+    def name
+      @city.asciiname.capitalize_phrase
+    end
+
+    def code
+      @city.city_code
+    end
+
+    def to_hash
+      { id: @city.id,
+        name: self.name,
+        subdivision_id: @city.subdivision.id}
+    end
+
+    def to_json
+      to_hash.to_json
+    end
+  end
+end
+
