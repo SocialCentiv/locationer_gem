111a491da4daa7cac79b6d2ced1c35e1866af1fa
Juan Leal <juan@hiplogiq.com>
2013-12-20 15:24:34 -0600

    removed grape api stuff

--- lib/locationer/engine.rb
+++ lib/locationer/engine.rb
@@ -1,9 +1,6 @@
 module Locationer
   class Engine < ::Rails::Engine
     isolate_namespace Locationer
-    require 'grape'
-    config.paths.add "app/api", glob: "**/*.rb"
-    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
 
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
--- locationer.gemspec
+++ locationer.gemspec
@@ -17,7 +17,6 @@ Gem::Specification.new do |s|
   s.test_files = Dir["spec/**/*"]
 
   s.add_dependency "rails", "~> 4.0"
-  s.add_dependency "grape"
   s.add_dependency "ruby-progressbar"
 
   s.add_development_dependency "pg"
