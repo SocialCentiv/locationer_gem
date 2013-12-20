ca48d0c683cc803a7fdacce4126ef3366acbe3e5
Juan Leal <juan@hiplogiq.com>
2013-12-19 23:24:49 -0600

    Add functionality

--- app/controllers/locationer/cities_controller.rb
+++ app/controllers/locationer/cities_controller.rb
@@ -2,7 +2,34 @@ require_dependency "locationer/application_controller"
 
 module Locationer
   class CitiesController < ApplicationController
+    respond_to :json
+    
     def index
+      validate_params
+      @cities = City.find_nearby_cities_around(*converted_params) if @errors.empty?
+      
+      if @cities
+        respond_with(CityJsonDecorator.cities_to_json(@cities))
+      end
+    end
+
+    private
+
+    def validate_params
+      @errors = []
+      @errors << "Required parameter of 'name' missing" unless params[:name]
+      @errors << "Required parameter of 'subdivision_code' missing" unless params[:subdivision_code]
+      @errors << "Required parameter of 'country_code' missing" unless params[:country_code]
+    end
+
+    def converted_params
+      center_city = params.fetch(:name)
+      subdivision_code = params.fetch(:subdivision_code)
+      country_code = params.fetch(:country_code)
+      radius = params.fetch(:radius){0.0}
+      [center_city, {"subdivision_code" => subdivision_code,
+        "country_code" => country_code,
+        "radius" => radius}]
     end
   end
 end
--- spec/controllers/locationer/cities_controller_spec.rb
+++ spec/controllers/locationer/cities_controller_spec.rb
@@ -2,6 +2,44 @@ require 'spec_helper'
 
 module Locationer
   describe CitiesController do
+    routes { Locationer::Engine.routes }
+    #only: :show
 
+    # This should return the minimal set of attributes required to create a valid
+    # cityTarget. As you add validations to cityTarget, be sure to
+    # adjust the attributes here as well.
+    let(:valid_attributes) { {  } }
+
+    # This should return the minimal set of values that should be in the session
+    # in order to pass any filters (e.g. authentication) defined in
+    # cityTargetsController. Be sure to keep this updated too.
+    let(:valid_session) { {} }
+
+    describe "GET index" do
+      before(:each) do
+        @texas = create :locationer_subdivision, :tx
+        @dallas = create :locationer_city, :dallas
+        @city_within_4_miles = create :locationer_city, :oak_cliff
+        @city_within_6_miles = create :locationer_city, :eagle_ford
+      end
+
+      it "assigns the requested city as @city" do
+        get :index, {:name => @dallas.asciiname, 
+          :subdivision_code => @dallas.admin1_code,
+          :country_code => @dallas.country_code,
+          :radius => 5.0, :format => :json}, valid_session
+        assigns(:city).should eq(@city)
+      end
+
+      it "should return json" do
+        get :index, {:name => @dallas.asciiname, 
+          :subdivision_code => @dallas.admin1_code,
+          :country_code => @dallas.country_code,
+          :radius => 5.0, :format => :json}, valid_session
+        response.body.should eql("[{\"id\":#{@dallas.id},\"name\":\"Dallas\",\"subdivision_id\":#{@dallas.subdivision.id}},{\"id\":#{@city_within_4_miles.id},\"name\":\"Oak Cliff\",\"subdivision_id\":#{@city_within_4_miles.subdivision.id}}]")
+      end      
+    end  
   end
 end
+
+
